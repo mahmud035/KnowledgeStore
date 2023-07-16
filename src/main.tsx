b04740a117feec822e4691f494dabac9f718adce
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-16 16:05:42 +0600

    build the project

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import store from './redux/store.ts';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
