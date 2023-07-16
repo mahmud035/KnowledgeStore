8871a1056bfc1e6fc150a2c5a8260c73fe45fc4b
mahmud035 <mahamudulhasan4148@gmail.com>
2023-07-16 08:31:32 +0600

    show a confirmation modal while deleting the book

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -8,8 +8,25 @@ import { Link, useParams } from 'react-router-dom';
 import { useGetBookDetailsQuery } from '../redux/features/books/bookApi';
 import { useAppSelector } from '../redux/hooks';
 import { AiOutlineHeart, AiOutlineRead } from 'react-icons/ai';
+import DeleteModal from '../components/DeleteModal';
+import { useState } from 'react';
 
 const BookDetails = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const modalHandler = (id: string) => {
+    console.log(id);
+    setIsOpen(false);
+  };
+
   const { id } = useParams();
   const { data: bookData } = useGetBookDetailsQuery(id, {
     refetchOnMountOrArgChange: true,
@@ -30,82 +47,88 @@ const BookDetails = () => {
 
   return (
     <div>
-      <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          {/* child-1 */}
-          <div className="card bg-base-100 shadow-lg">
-            <div className="card-body">
-              <h2 className="card-title">Title: {title}</h2>
-              <p>Author: ${author}</p>
-              <p>Genre: {genre}</p>
-              <p>PublishYear: {publishYear}</p>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        {/* child-1 */}
+        <div className="card bg-base-100 shadow-lg">
+          <div className="card-body">
+            <h2 className="card-title">Title: {title}</h2>
+            <p>Author: ${author}</p>
+            <p>Genre: {genre}</p>
+            <p>PublishYear: {publishYear}</p>
 
+            <div className="flex gap-4">
+              <div className="card-actions mt-2">
+                <Link to={`/book-details/${_id}`}>
+                  <button className="flex items-center gap-3 rounded-full border-0 pr-5 py-2 font-semibold capitalize outline-none transition duration-500 ease-in-out  hover:text-[#DA9323]">
+                    <AiOutlineHeart size={24} /> Add to Wishlist
+                  </button>
+                </Link>
+              </div>
+              <div className="card-actions mt-2">
+                <Link to={`/book-details/${_id}`}>
+                  <button className="flex items-center gap-3  rounded-full border-0 font-semibold px-5 py-2 capitalize outline-none transition duration-500 ease-in-out  hover:text-[#DA9323]">
+                    <AiOutlineRead size={24} /> Add to Reading List
+                  </button>
+                </Link>
+              </div>
+            </div>
+
+            {email === savedEmail && (
               <div className="flex gap-4">
                 <div className="card-actions mt-2">
-                  <Link to={`/book-details/${_id}`}>
-                    <button className="flex items-center gap-3 rounded-full border-0 pr-5 py-2 font-semibold capitalize outline-none transition duration-500 ease-in-out  hover:text-[#DA9323]">
-                      <AiOutlineHeart size={24} /> Add to Wishlist
+                  <Link to={`/edit-book/${_id}`}>
+                    <button className="rounded-full border-0 bg-[#DA9323]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#DA9323] hover:bg-transparent hover:text-[#DA9323]">
+                      Edit Book
                     </button>
                   </Link>
                 </div>
                 <div className="card-actions mt-2">
-                  <Link to={`/book-details/${_id}`}>
-                    <button className="flex items-center gap-3  rounded-full border-0 font-semibold px-5 py-2 capitalize outline-none transition duration-500 ease-in-out  hover:text-[#DA9323]">
-                      <AiOutlineRead size={24} /> Add to Reading List
-                    </button>
-                  </Link>
+                  <button
+                    onClick={openModal}
+                    className="rounded-full border-0 bg-[#da2378]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#da23b2] hover:bg-transparent hover:text-[#DA9323]"
+                  >
+                    Delete Book
+                  </button>
                 </div>
               </div>
-
-              {email === savedEmail && (
-                <div className="flex gap-4">
-                  <div className="card-actions mt-2">
-                    <Link to={`/edit-book/${_id}`}>
-                      <button className="rounded-full border-0 bg-[#DA9323]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#DA9323] hover:bg-transparent hover:text-[#DA9323]">
-                        Edit Book
-                      </button>
-                    </Link>
-                  </div>
-                  <div className="card-actions mt-2">
-                    <Link to={`/book-details/${_id}`}>
-                      <button className="rounded-full border-0 bg-[#da2378]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#da23b2] hover:bg-transparent hover:text-[#DA9323]">
-                        Delete Book
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </div>
+            )}
           </div>
-          {/* child-2 */}
-          <div className="card bg-base-100 shadow-lg">
-            <div className="card-body">
-              <h2 className="card-title">Reviews:</h2>
-              {reviews?.map((review: string, index: string) => (
-                <p key={index} className="">
-                  {index + 1}. {review}
-                </p>
-              ))}
-            </div>
+        </div>
+        {/* child-2 */}
+        <div className="card bg-base-100 shadow-lg">
+          <div className="card-body">
+            <h2 className="card-title">Reviews:</h2>
+            {reviews?.map((review: string, index: string) => (
+              <p key={index} className="">
+                {index + 1}. {review}
+              </p>
+            ))}
           </div>
-          {/* child-3 */}
-          <div className="lg:col-span-2 w-full lg:w-1/2 mx-auto card bg-base-100 shadow-lg">
-            <div className="card-body ">
-              <textarea
-                placeholder="Give your valuable review here..."
-                className="textarea textarea-bordered textarea-lg w-full max-w-full h-full"
-              ></textarea>
-            </div>
-            <div className="card-actions mb-6 mx-auto">
-              <Link to={`/book-details/${_id}`}>
-                <button className="rounded-full border-0 bg-[#DA9323]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#DA9323] hover:bg-transparent hover:text-[#DA9323]">
-                  Submit Review
-                </button>
-              </Link>
-            </div>
+        </div>
+        {/* child-3 */}
+        <div className="lg:col-span-2 w-full lg:w-1/2 mx-auto card bg-base-100 shadow-lg">
+          <div className="card-body ">
+            <textarea
+              placeholder="Give your valuable review here..."
+              className="textarea textarea-bordered textarea-lg w-full max-w-full h-full"
+            ></textarea>
+          </div>
+          <div className="card-actions mb-6 mx-auto">
+            <Link to={`/book-details/${_id}`}>
+              <button className="rounded-full border-0 bg-[#DA9323]  px-5 py-2 capitalize text-white outline-none transition duration-500 ease-in-out hover:border hover:border-[#DA9323] hover:bg-transparent hover:text-[#DA9323]">
+                Submit Review
+              </button>
+            </Link>
           </div>
         </div>
       </div>
+
+      <DeleteModal
+        isOpen={isOpen}
+        closeModal={closeModal}
+        id={_id}
+        modalHandler={modalHandler}
+      />
     </div>
   );
 };
